bce58d6f87eb065a81d9dc43df87a8db12cadb9c
hpf3 <18592150+hpf3@users.noreply.github.com>
2025-09-07 20:25:07 -0700

    undo App.tsx edit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,39 @@
+import { Fragment } from 'preact'
+import type { RouterOnChangeArgs } from 'preact-router'
+import { Router } from 'preact-router'
 import '../styles/global.css'
 import '../styles/nodes.css'
+import { Analytics } from './Analytics.js'
+import { Header } from './components/index.js'
+import { Changelog, Convert, Customized, Generator, Generators, Guide, Guides, Home, LegacyPartners, Partners, Sounds, Transformation, Versions, WhatsNew, Worldgen } from './pages/index.js'
+import { cleanUrl } from './Utils.js'
 
 export function App() {
+	const changeRoute = (e: RouterOnChangeArgs) => {
+		window.dispatchEvent(new CustomEvent('replacestate'))
+		// Needs a timeout to ensure the title is set correctly
+		setTimeout(() => Analytics.pageview(cleanUrl(e.url)))
+	}
 
-	return <></>
+	return <Fragment>
+		<Header />
+		<Router onChange={changeRoute}>
+			<Home path="/" />
+			<Generators path="/generators" />
+			<Worldgen path="/worldgen" />
+			<Partners path="/partners" />
+			<LegacyPartners path="/partners/:id" />
+			<Sounds path="/sounds" />
+			<Changelog path="/changelog" />
+			<Versions path="/versions" />
+			<Transformation path="/transformation" />
+			<Customized path="/customized" />
+			<Convert path="/convert" />
+			<Convert path="/convert/:formats" />
+			<WhatsNew path="/whats-new" />
+			<Guides path="/guides" />
+			<Guide path="/guides/:id" />
+			<Generator default />
+		</Router>
+	</Fragment>
 }
